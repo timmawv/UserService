ab4715eda717ef3587e950141626387a3afdcf86
Timur <avlyakulovtimur@gmail.com>
2024-05-04 19:03:25 +0300

    initial commit

---
+++ src/test/java/avlyakulov/timur/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package avlyakulov.timur.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
